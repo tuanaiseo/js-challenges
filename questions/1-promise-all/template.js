ab022bc11c666488292a525e771efbc85973a822
tuanaiseo <tuanaiseo@gmail.com>
2026-04-06 18:19:02 +0700

    refactor: core challenge implementation is missing in templa
    `MyPromise.all` is declared but has no implementation, so challenge submissions based on this template cannot pass behavior requirements and may mislead contributors about expected API behavior.
    
    Affected files: template.js
    
    Signed-off-by: tuanaiseo <221258316+tuanaiseo@users.noreply.github.com>

--- questions/1-promise-all/template.js
+++ questions/1-promise-all/template.js
@@ -1,5 +1,32 @@
 export default function (MyPromise) {
-    MyPromise.all = function () {
+    MyPromise.all = function (iterable) {
+        return new MyPromise(function (resolve, reject) {
+            var results = []
+            var remaining = 0
+            var index = 0
 
+            try {
+                for (var item of iterable) {
+                    (function (currentIndex) {
+                        remaining++
+                        MyPromise.resolve(item).then(function (value) {
+                            results[currentIndex] = value
+                            remaining--
+                            if (remaining === 0) {
+                                resolve(results)
+                            }
+                        }, reject)
+                    })(index)
+                    index++
+                }
+            } catch (error) {
+                reject(error)
+                return
+            }
+
+            if (index === 0) {
+                resolve([])
+            }
+        })
     }
 }
